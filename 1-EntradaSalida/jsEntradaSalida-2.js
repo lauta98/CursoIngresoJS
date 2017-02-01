90fcdc8d3ca15a0eb8ba67bc34fa02acefeb51d5
lauta98 <lautacapo7rey@hotmail.com>
2017-02-01 12:22:17 -0300

    2 hasta el 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,6 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	var valor= prompt("Escribir algo");
-	var alert= prompt(alert);
+	var valor= prompt()
+	alert(valor);
 }
