e5a715c896e146ca08de2159cd109c9649b6c647
lauta98 <lautacapo7rey@hotmail.com>
2017-02-02 12:19:40 -0300

    9
    Bien

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var resultado;
 
+	sueldo=document.getElementById('sueldo').value;
 	
-}
+	sueldo=parseInt(sueldo);
+	resultado=sueldo*0.1;
+
+	document.getElementById('resultado').value=resultado;
+
+
+	}
